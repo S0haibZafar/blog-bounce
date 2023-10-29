327bfe20442ac1fe497887338ce4414641950325
S0haibZafar <sohaibzafar71@gmail.com>
2023-10-29 23:14:55 +0500

    Resolve error by installing bodyParser

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+var bodyParser = require('body-parser');
 const dbConnect = require('./database/index');
 const {PORT} = require('./config/index')
 const router = require('./routes/index') 
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.use(router);
 
