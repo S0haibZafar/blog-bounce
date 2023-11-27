3ca5871c8e8dca6e0ac69d1a312cf82c41a9bc49
Sohaib-Zafar <100518647+s0haibzafar@users.noreply.github.com>
2023-11-27 09:12:47 +0500

    Create blog post route and handle image as well as store in db (#6)
    * Create blog post route and handle image as well as store in db
    
    * Create a route to get all blogs
    
    * Images
    
    * Create route for GetBlogById
    
    * Also same route to get blog detail as well as author detail

---
+++ controller/blogController.js
@@ -0,0 +1,117 @@
+const Joi = require('joi');
+const fs = require("fs");
+const Blog = require('../models/blog');
+const {BACKEND_SERVER_PATH} = require('../config/index');
+const BlogDTO = require("../dto/blog")
+const BlogDetailsDTO = require("../dto/blogDetails")
+
+const mongodbIdPattran = /^[0-9a-fA-F]{24}$/;
+
+const blogController = {
+    async create(req, res, next) {
+        //validate req body
+        //handler photo  storage , save path onto db
+        //add in db
+        //response
+
+        //client side photo -> base64 encoding string -> decode  -> store ->save photo's path into db
+        const createBlogSchema = Joi.object({
+            title: Joi.string().required(),
+            author: Joi.string().regex(mongodbIdPattran).required(),
+            content: Joi.string().required(),
+            photo: Joi.string().required(),
+        })
+
+        const {error} = createBlogSchema.validate(req.body)
+
+        if (error) {
+            return next(error);
+        }
+
+        const { title, author, content, photo } = req.body;
+
+        //read as buffer
+        const buffer = Buffer.from(photo.replace(/^data:image\/(png|jpg|jpeg);base64,/, ''), 'base64')
+        //randing
+        const imagePath = `${Date.now()}-${author}.png`;
+        //save locally
+        try {
+            fs.writeFileSync(`storage/${imagePath}`, buffer);
+        }
+        catch (e) {
+            return next(e);
+        }
+
+        //save blog in db
+        let newBlog
+        try {
+             newBlog = new Blog({
+                title,
+                author,
+                content,
+                photoPath: `${BACKEND_SERVER_PATH}/storage/${imagePath}`
+            })
+
+            await newBlog.save(newBlog);
+        }
+        catch (error) {
+            return next(error);
+        }
+
+        const blogDto = new BlogDTO(newBlog)
+
+        return res.status(201).json({blog:blogDto});
+
+    },
+    async getAll(req, res, next) { 
+
+        const blogs = [];
+        try{
+            const blogList= await Blog.find({});
+
+            for(let i = 0; i < blogList.length; i++ ){
+                const dto = new  BlogDTO(blogList[i])
+                blogs.push(dto);
+            }
+
+            return res.status(200).json({blog:blogs });
+
+        }
+        catch(e){
+            return next(e);
+        }
+    },
+    async getById(req, res, next) { 
+
+        const getBlogByIdSchema = Joi.object({
+            id : Joi.string().regex(mongodbIdPattran).required()
+        })
+
+        const {error} = getBlogByIdSchema.validate(req.params);
+
+        if(error){
+            return next(error)
+        }
+
+        const blogId = req.params.id;
+        
+        let blog;
+        try{
+            blog= await Blog.findOne({_id: blogId}).populate('author');
+        }
+        catch(e){
+            return next(e)
+        }
+
+         const blogdto = new BlogDetailsDTO(blog);
+
+        return res.status(200).json({blog: blogdto})
+
+
+    },
+    // async update(req, res, next) { },
+    // async delete(req, res, next) { },
+
+}
+
+module.exports = blogController;
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 const authController  = require('../controller/authController')
 
+const blogController  = require('../controller/blogController')
+
 
 router.get('/test', (req, res) => res.json({ msg: "Working !"}));
 
@@ -23,10 +25,15 @@ router.get('/refresh', authController.refresh);
 
 //blod CURD
 //create
+router.post('/blog', auth, blogController.create);
 //read all blogs
+router.get('/blog/all', auth, blogController.getAll);
 // blog detail by id
+router.get('/blog/:id', auth, blogController.getById);
 //update
+// router.put('/blog', auth, blogController.update);
 //delete
+// router.delete('/blog/:id', auth, blogController.delete);
 
 
 //comment
