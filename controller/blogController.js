e502a8aba79f5978a0031f9bb3533c37c30bc1f5
S0haibZafar <sohaibzafar71@gmail.com>
2023-12-07 22:20:34 +0500

    Add route to update blog data

--- controller/blogController.js
+++ controller/blogController.js
@@ -109,7 +109,68 @@ const blogController = {
 
 
     },
-    // async update(req, res, next) { },
+    async update(req, res, next) { 
+        // validate 
+        //if we update whole info then we need to delete previous photo and update. otherwise we do't delete photo
+        
+        const updateBlogSchema = Joi.object({
+            title: Joi.string().required(),
+            content: Joi.string().required(),
+            author: Joi.string().regex(mongodbIdPattran).required(),
+            blogId: Joi.string().regex(mongodbIdPattran).required(),
+            photo: Joi.string(),
+        })
+
+        const {error} = updateBlogSchema.validate(req.body)
+
+        // if (error) {
+        //     return next(error);
+        // }
+
+        const { title, content, author, blogId, photo } = req.body;
+
+        //delete previous photo
+        //other wise save news photo
+        let blog;
+
+        try{
+            blog = await Blog.findOne({_id: blogId})
+        }
+        catch(e){
+            return next(e)
+        }
+
+        if(photo){
+            let previousPhoto =  blog.photoPath;
+
+            previousPhoto = previousPhoto.split('/').at(-1);
+            //delete photo
+            fs.unlinkSync(`storage/${previousPhoto}`)
+
+            //Now we save new photo
+             //read as buffer
+            const buffer = Buffer.from(photo.replace(/^data:image\/(png|jpg|jpeg);base64,/, ''), 'base64')
+            //randing
+            const imagePath = `${Date.now()}-${author}.png`;
+            //save locally
+            try {
+                fs.writeFileSync(`storage/${imagePath}`, buffer);
+            }
+            catch (e) {
+                return next(e);
+            }   
+
+            await Blog.updateOne({_id: blogId},{
+                title, content, photoPath: `${BACKEND_SERVER_PATH}/storage/${imagePath}`
+            })
+
+        }else{
+            //if we do't need to update photo only need to update content 
+            await Blog.updateOne({_id: blogId}, {title, content});
+        }
+        return res.status(200).json({message : "blog updated!"});
+
+    },
     // async delete(req, res, next) { },
 
 }
--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ router.get('/blog/all', auth, blogController.getAll);
 // blog detail by id
 router.get('/blog/:id', auth, blogController.getById);
 //update
-// router.put('/blog', auth, blogController.update);
+router.put('/blog', auth, blogController.update);
 //delete
 // router.delete('/blog/:id', auth, blogController.delete);
 
