c53790f0ac2178e2cf2e8f36aa2cb0646e1b045e
S0haibZafar <sohaibzafar71@gmail.com>
2023-12-12 22:59:15 +0500

    create route for getComment by id

--- controller/commentController.js
+++ controller/commentController.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 const Comment = require('../models/comment');
+const commentDTO = require('../dto/comments');
 
 const mongodbIdPattran = /^[0-9a-fA-F]{24}$/;
 
@@ -35,7 +36,39 @@ const commentController = {
         return res.status(200).json({message: "Comment Created!"})
 
     },
-    async getById(req, res, next) { }
+    async getById(req, res, next) { 
+        const getByIdSchema = Joi.object({
+            id: Joi.string().regex(mongodbIdPattran).required(),
+        })
+
+        const {error} = getByIdSchema.validate(req.params);
+
+        if(error){
+            return next(error);
+        }
+
+        const {id} = req.params;
+
+        let comments;
+        try{
+            comments = await Comment.find({blog: id}).populate('author')
+
+        }
+        catch(e){
+            return next(e);
+        }
+
+        let commentDto = []
+        for(let i = 0; i < comments.length; i++){
+            const obj = new commentDTO(comments[i]);
+            commentDto.push(obj)
+
+        }
+
+        return res.status(200).json({data: commentDto})
+
+
+    }
 }
 
 module.exports = commentController;
